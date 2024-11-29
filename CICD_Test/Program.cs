3467fdcb3c48b0aa6e74a32ea2ea1331d6a44d40
xeno ceph <xenoceph@gmail.com>
2024-11-29 11:11:46 +0800

    test

--- CICD_Test/Program.cs
+++ CICD_Test/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! Test run CICD deployment workflow");
 
+while(true)
+{
+    Console.WriteLine("Task Running..");
+    await Task.Delay(1000);
+}
+
 Console.ReadLine();
